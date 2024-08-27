4dc5db46a425291f09f5f33d35328213c59961ce
Jordach Makaya H.M <4thinkai@gmail.com>
2024-08-27 17:54:08 +0100

    First Version

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,8 @@
+
+8
+index.js,a\7\a77b15c0d64cf6111e025645e562faf8ccabb9d7
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
+<
+package.json,7\0\7030d0b2f71b999ff89a343de08c414af32fc93a
---
+++ index.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+console.log(`
+  Jordach M@kaya 🚀
+  
+  Jordach Makaya is a skilled software developer specializing in NestJS, TypeORM, and Angular, with a focus on building 
+  robust, scalable applications. He excels in creating modular codebases within monorepo structures and optimizing CI/CD 
+  pipelines with GitHub Actions and Vercel. Jordach is experienced in TDD, complex business logic implementation, and 
+  managing databases like MySQL and PostgreSQL. He is committed to code quality, using tools like SonarCloud, and is an 
+  effective communicator, making complex technical concepts accessible.
+  
+  🔗   Linkedin: https://www.linkedin.com/in/jordachmakaya/
+  📖   GitHub :  https://github.com/TheGreatJordach
+  
+  
+  `)
